f7e19aea2dc4888eb5b404c8105fdedf7b081af3
Pavan0409 <pavanchitaguppa@gmail.com>
2022-09-16 02:50:25 +0530

    toggle, increase and decrease are optimised way

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,15 +1,37 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const counterReducer = (state = { counter: 0, showcounter: false }, action) => {
   if (action.type === "increment") {
     return {
-      counter: state.counter + 5,
+      counter: state.counter + 1,
+      showcounter: state.showcounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
-      counter: state.counter - 5,
+      counter: state.counter - 1,
+      showcounter: state.showcounter,
+    };
+  }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.value,
+      showcounter: state.showcounter,
+    };
+  }
+  if (action.type === "decrease") {
+    return {
+      counter: state.counter - action.value,
+      showcounter: state.showcounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showcounter: !state.showcounter,
+      counter: state.counter,
     };
   }
 
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,6 +5,7 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showcounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
@@ -12,16 +13,26 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
+  const increaseHandler = () => {
+    dispatch({ type: "increase", value: 5 });
+  };
+  const decreaseHandler = () => {
+    dispatch({ type: "decrease" , value: 5 });
+  };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle"})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decreaseHandler}>Decrease by 5</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
