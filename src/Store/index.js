6cd275365c75ce2ce05f3eb4a4cfeca2595b1b1e
Pavan0409 <pavanchitaguppa@gmail.com>
2022-09-16 02:01:42 +0530

    Increment and decrement counter by 5

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,20 +1,20 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0}, action) => {
-    if(action.type === 'increment'){
-        return{
-            counter: state.counter+2,
-        };
-    }
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 5,
+    };
+  }
 
-    if(action.type === 'decrement'){
-        return{
-            counter: state.counter-2,
-        };
-    }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 5,
+    };
+  }
 
-    return state;
-}
+  return state;
+};
 
 const store = createStore(counterReducer);
 
